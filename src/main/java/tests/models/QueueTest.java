96454516576481c83b04a80638d6ee8366fcd3fa
Alex Knowlton <alexrules313@gmail.com>
2019-12-04 14:11:47 -0500

    added skeleton classes for session test and queue test

---
+++ src/main/java/tests/models/QueueTest.java
@@ -0,0 +1,68 @@
+package tests.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QueueTest {
+
+    @Test
+    public void testRecommendSong() {
+    }
+
+    @Test
+    public void testAdjustTimeScore() {
+    }
+
+    @Test
+    public void testAddPlayedSongId() {
+    }
+
+    @Test
+    public void testGetPlayedSongIdsAsString() {
+    }
+
+    @Test
+    public void testPopNextSong() {
+    }
+
+    @Test
+    public void testToDocument() {
+    }
+
+    @Test
+    public void testFromDocument() {
+    }
+
+    @Test
+    public void testGetNumSongsAddedPastHourForUser() {
+    }
+
+    @Test
+    public void testGetSetQueueId() {
+    }
+
+    @Test
+    public void testGetSetScore() {
+    }
+
+    @Test
+    public void testGetSetPlayedSongIds() {
+    }
+
+    @Test
+    public void testGetSetSongs() {
+    }
+
+    @Test
+    public void testGetSetMinTime() {
+    }
+
+    @Test
+    public void testGetSetMaxTime() {
+    }
+
+    @Test
+    public void testGetSetSessionId() {
+    }
+}
---
+++ src/main/java/tests/models/SessionTest.java
@@ -0,0 +1,60 @@
+package tests.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SessionTest {
+
+    @Test
+    public void toDocument() {
+    }
+
+    @Test
+    public void fromDocument() {
+    }
+
+    @Test
+    public void testGetSetSessionId() {
+    }
+
+    @Test
+    public void testGetSetName() {
+    }
+
+    @Test
+    public void testGetSetDjId() {
+    }
+
+    @Test
+    public void testGetSetPartierIds() {
+    }
+
+    @Test
+    public void testGetSetCurrentSong() {
+    }
+
+    @Test
+    public void testGetSetSettingId() {
+    }
+
+    @Test
+    public void testGetSetQueueId() {
+    }
+
+    @Test
+    public void testGetSetAccessToken() {
+    }
+
+    @Test
+    public void testGetSetRefreshToken() {
+    }
+
+    @Test
+    public void testGetSetState() {
+    }
+
+    @Test
+    public void testGetSetLastUpdated() {
+    }
+}
