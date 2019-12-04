ade64b5efa0e07225d6bd1c12eaf813d592d3327
Alex Knowlton <alexrules313@gmail.com>
2019-12-04 10:56:46 -0500

    added user test

---
+++ src/main/java/tests/models/UserTest.java
@@ -0,0 +1,154 @@
+package tests.models;
+
+import jhu.group6.sounDJam.models.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(User.class)
+public class UserTest {
+    private UUID userId = UUID.fromString("098ade23-807f-12e9-6183-c9a862e9a86b");
+    private UUID sessionId = UUID.fromString("098ade23-807f-12e9-6183-a1d836c3b37e");
+    private String nickname = "nickname";
+    private int numSongsAdded = 100;
+    private int numBoos = 40;
+
+    @Before
+    public void Before() {
+        mockStatic(UUID.class);
+        when(UUID.randomUUID()).thenReturn(userId);
+        when(UUID.fromString(any(String.class))).thenReturn(userId).thenReturn(sessionId);
+    }
+
+    @Test
+    public void testIncrementNumSongsAdded() {
+        var user = User.builder().build();
+
+        assertEquals(user.getNumSongsAdded(), 0);
+        user.incrementNumSongsAdded();
+        assertEquals(1, user.getNumSongsAdded());
+    }
+
+    @Test
+    public void testIncrementNumBoos() {
+        var user = User.builder().build();
+
+        assertEquals(user.getNumBoos(), 0);
+        user.incrementNumBoos();
+        assertEquals(1, user.getNumBoos());
+    }
+
+    @Test
+    public void testToDocument() {
+        var userFull = User.builder()
+                .nickname(nickname)
+                .sessionId(sessionId)
+                .numSongsAdded(numSongsAdded)
+                .numBoos(numBoos)
+                .build()
+                .toDocument();
+
+        assertEquals(userFull.get("nickname"), nickname);
+        assertEquals(userFull.get("sessionId"), sessionId.toString());
+        assertEquals(userFull.get("numSongsAdded"), numSongsAdded);
+        assertEquals(userFull.get("numBoos"), numBoos);
+        assertEquals(userFull.get("userId"), userId.toString());
+
+        when(UUID.randomUUID()).thenReturn(userId);
+        when(UUID.fromString(any(String.class))).thenReturn(userId);
+
+        var userEmpty = User.builder()
+                .build()
+                .toDocument();
+
+        assertNull(userEmpty.get("nickname"));
+        assertNull(userEmpty.get("sessionId"));
+        assertEquals(userEmpty.get("numSongsAdded"), 0);
+        assertEquals(userEmpty.get("numBoos"), 0);
+        assertEquals(userEmpty.get("userId"), userId.toString());
+    }
+
+    @Test
+    public void testFromDocument() {
+        var userFull = User.builder()
+                .nickname(nickname)
+                .sessionId(sessionId)
+                .numSongsAdded(numSongsAdded)
+                .numBoos(numBoos)
+                .build()
+                .toDocument();
+
+        var userDocFull = User.fromDocument(userFull);
+
+        assertEquals(userDocFull.getUserId(), userId);
+        assertEquals(userDocFull.getNumSongsAdded(), numSongsAdded);
+        assertEquals(userDocFull.getNumBoos(), numBoos);
+        assertEquals(userDocFull.getSessionId(), sessionId);
+        assertEquals(userDocFull.getNickname(), nickname);
+
+        when(UUID.randomUUID()).thenReturn(userId);
+        when(UUID.fromString(any(String.class))).thenReturn(userId);
+
+        var userEmpty = User.builder()
+                .build()
+                .toDocument();
+
+        var userDocEmpty = User.fromDocument(userEmpty);
+
+        assertEquals(userDocEmpty.getUserId(), userId);
+        assertEquals(userDocEmpty.getNumSongsAdded(), 0);
+        assertEquals(userDocEmpty.getNumBoos(), 0);
+        assertNull(userDocEmpty.getSessionId());
+        assertNull(userDocEmpty.getNickname());
+    }
+
+    @Test
+    public void testGetSetNickname() {
+        var user = User.builder().build();
+        assertNull(user.getNickname());
+        user.setNickname(nickname);
+        assertEquals(nickname, user.getNickname());
+    }
+
+    @Test
+    public void testGetSetSessionId() {
+        var user = User.builder().build();
+        assertNull(user.getSessionId());
+        user.setSessionId(sessionId);
+        assertEquals(sessionId, user.getSessionId());
+    }
+
+    @Test
+    public void testGetSetUserId() {
+        var user = User.builder().build();
+        assertEquals(userId, user.getUserId());
+        user.setUserId(sessionId);
+        assertEquals(sessionId, user.getUserId());
+    }
+
+    @Test
+    public void testGetSetNumSongsAdded() {
+        var user = User.builder().build();
+        assertEquals(0, user.getNumSongsAdded());
+        user.setNumSongsAdded(numSongsAdded);
+        assertEquals(numSongsAdded, user.getNumSongsAdded());
+    }
+
+    @Test
+    public void testGetSetNumBoos() {
+        var user = User.builder().build();
+        assertEquals(0, user.getNumBoos());
+        user.setNumBoos(numBoos);
+        assertEquals(numBoos, user.getNumBoos());
+    }
+}
