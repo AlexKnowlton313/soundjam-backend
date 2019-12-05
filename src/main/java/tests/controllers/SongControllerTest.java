b1598b787928f023d606ad287acdafa6dfe384bf
Alex Knowlton <alexrules313@gmail.com>
2019-12-05 00:05:40 -0500

    added user controller test

---
+++ src/main/java/tests/controllers/SongControllerTest.java
@@ -0,0 +1,12 @@
+package tests.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SongControllerTest {
+
+    @Test
+    public void getSongFromContext() {
+    }
+}
