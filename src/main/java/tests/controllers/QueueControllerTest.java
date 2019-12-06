523132597db07b4410574d53541c1e91322dbcce
Alex Knowlton <alexrules313@gmail.com>
2019-12-06 08:26:14 -0500

    finished setting controller tests

---
+++ src/main/java/tests/controllers/QueueControllerTest.java
@@ -0,0 +1,32 @@
+package tests.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QueueControllerTest {
+
+    @Test
+    public void getQueue() {
+    }
+
+    @Test
+    public void curateQueue() {
+    }
+
+    @Test
+    public void recommendSong() {
+    }
+
+    @Test
+    public void getQueueFromContext() {
+    }
+
+    @Test
+    public void getQueueFromId() {
+    }
+
+    @Test
+    public void getGraph() {
+    }
+}
--- src/main/java/tests/controllers/SettingControllerTest.java
+++ src/main/java/tests/controllers/SettingControllerTest.java
@@ -1,10 +1,14 @@
 package tests.controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.Context;
 import jhu.group6.sounDJam.Server;
 import jhu.group6.sounDJam.controllers.HealthCheckController;
+import jhu.group6.sounDJam.controllers.QueueController;
 import jhu.group6.sounDJam.controllers.SettingController;
 import jhu.group6.sounDJam.exceptions.InvalidSessionIdException;
+import jhu.group6.sounDJam.models.Queue;
 import jhu.group6.sounDJam.models.Setting;
 import jhu.group6.sounDJam.models.Song;
 import jhu.group6.sounDJam.repositories.MongoRepository;
@@ -12,19 +16,22 @@ import org.bson.Document;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Server.class})
+@PrepareForTest({Server.class, QueueController.class, Setting.class})
 public class SettingControllerTest {
 
     @Test(expected = InvalidSessionIdException.class)
@@ -89,14 +96,141 @@ public class SettingControllerTest {
     }
 
     @Test
-    public void postSetting() {
+    public void postSettingAllTrue() throws IOException {
+        var valueCaptureStatus = ArgumentCaptor.forClass(Integer.class);
+
+        PowerMockito.mockStatic(Server.class);
+        PowerMockito.mockStatic(QueueController.class);
+
+        var contextMock = mock(Context.class);
+        var mongoMock = mock(MongoRepository.class);
+        var settingMock = mock(Setting.class);
+        var objectMapperMock = mock(ObjectMapper.class);
+        var jsonNodeMock = mock(JsonNode.class);
+        var queueMock = mock(Queue.class);
+
+        var jsonList = new ArrayList<JsonNode>();
+        jsonList.add(jsonNodeMock);
+
+        var songList = new ArrayList<Song>() {{ add(Song.builder().build()); }};
+
+        // getSettingFromContext
+        when(contextMock.sessionAttribute(anyString())).thenReturn(null).thenReturn(settingMock);
+        when(contextMock.pathParam(anyString())).thenReturn("id");
+        PowerMockito.when(Server.getMongoRepository())
+                .thenReturn(mongoMock).thenReturn(mongoMock).thenReturn(mongoMock);
+        when(mongoMock.findOneFromCollectionBySessionId(any(), anyString())).thenReturn(new Document());
+
+        // our method
+        PowerMockito.when(Server.getJson()).thenReturn(objectMapperMock);
+        when(contextMock.body()).thenReturn("testContextBody");
+        when(objectMapperMock.readTree(anyString())).thenReturn(jsonNodeMock);
+        when(jsonNodeMock.get(anyString()))
+                .thenReturn(jsonNodeMock)
+                .thenReturn(jsonNodeMock)
+                .thenReturn(jsonNodeMock)
+                .thenReturn(jsonNodeMock)
+                .thenReturn(jsonNodeMock)
+                .thenReturn(jsonNodeMock)
+                .thenReturn(jsonNodeMock)
+                .thenReturn(jsonNodeMock);
+        doNothing().when(settingMock).setMinSongLength(anyInt());
+        doNothing().when(settingMock).setMaxSongLength(anyInt());
+        doNothing().when(settingMock).setValence(anyDouble());
+        doNothing().when(settingMock).setEnergy(anyDouble());
+        doNothing().when(settingMock).setDanceability(anyDouble());
+        doNothing().when(settingMock).setTempo(anyDouble());
+        doNothing().when(settingMock).setMaxUserCanAdd(anyInt());
+        doNothing().when(settingMock).setBlacklist(any());
+        when(jsonNodeMock.asInt())
+                .thenReturn(1)
+                .thenReturn(2)
+                .thenReturn(7);
+        when(jsonNodeMock.asDouble())
+                .thenReturn(3.0)
+                .thenReturn(4.0)
+                .thenReturn(5.0)
+                .thenReturn(6.0);
+        when(jsonNodeMock.iterator()).thenReturn(jsonList.iterator());
+        when(jsonNodeMock.asText()).thenReturn("artist");
+        PowerMockito.when(QueueController.getQueueFromContext(any())).thenReturn(queueMock);
+        when(queueMock.getSongs()).thenReturn(songList);
+
+        // updateQueueAfterSetting
+        PowerMockito.when(QueueController.getQueueFromContext(any())).thenReturn(queueMock);
+        PowerMockito.when(QueueController.curateQueue(queueMock, settingMock)).thenReturn(songList);
+        doNothing().when(queueMock).setSongs(any());
+        when(contextMock.pathParam(anyString())).thenReturn("testString");
+        when(queueMock.toDocument()).thenReturn(new Document());
+        when(mongoMock.updateOneFromCollectionBySessionId(any(), anyString(), any())).thenReturn(true);
+
+        SettingController.postSetting(contextMock);
+
+        verify(contextMock).status(valueCaptureStatus.capture());
+        assertEquals(valueCaptureStatus.getValue(), 204, 0);
     }
 
     @Test
-    public void getSettingFromContext() {
+    public void postSettingAllFalse() throws IOException {
+        var valueCaptureStatus = ArgumentCaptor.forClass(Integer.class);
+
+        PowerMockito.mockStatic(Server.class);
+        PowerMockito.mockStatic(QueueController.class);
+
+        var contextMock = mock(Context.class);
+        var mongoMock = mock(MongoRepository.class);
+        var settingMock = mock(Setting.class);
+        var objectMapperMock = mock(ObjectMapper.class);
+        var jsonNodeMock = mock(JsonNode.class);
+        var queueMock = mock(Queue.class);
+
+        var songList = new ArrayList<Song>();
+
+        // getSettingFromContext
+        when(contextMock.sessionAttribute(anyString())).thenReturn(null).thenReturn(settingMock);
+        when(contextMock.pathParam(anyString())).thenReturn("id");
+        PowerMockito.when(Server.getMongoRepository())
+                .thenReturn(mongoMock).thenReturn(mongoMock).thenReturn(mongoMock);
+        when(mongoMock.findOneFromCollectionBySessionId(any(), anyString())).thenReturn(new Document());
+
+        // our method
+        PowerMockito.when(Server.getJson()).thenReturn(objectMapperMock);
+        when(contextMock.body()).thenReturn("testContextBody");
+        when(objectMapperMock.readTree(anyString())).thenReturn(jsonNodeMock);
+        when(jsonNodeMock.get(anyString()))
+                .thenReturn(null)
+                .thenReturn(null)
+                .thenReturn(null)
+                .thenReturn(null)
+                .thenReturn(null)
+                .thenReturn(null)
+                .thenReturn(null)
+                .thenReturn(null);
+        PowerMockito.when(QueueController.getQueueFromContext(any())).thenReturn(queueMock);
+        when(queueMock.getSongs()).thenReturn(songList);
+
+        SettingController.postSetting(contextMock);
+
+        verify(contextMock).status(valueCaptureStatus.capture());
+        assertEquals(valueCaptureStatus.getValue(), 204, 0);
     }
 
     @Test
     public void getSettingFromId() {
+        PowerMockito.mockStatic(Server.class);
+        PowerMockito.mockStatic(Setting.class);
+
+        var mongoMock = mock(MongoRepository.class);
+        var settingMock = mock(Setting.class);
+
+        var settingDoc = new Document();
+
+        PowerMockito.when(Server.getMongoRepository()).thenReturn(mongoMock);
+        when(mongoMock.findOneFromCollectionBySessionId(any(), anyString())).thenReturn(settingDoc);
+        PowerMockito.when(Setting.fromDocument(settingDoc)).thenReturn(settingMock);
+
+        var ret = SettingController.getSettingFromId("test");
+
+        assertEquals(ret, settingMock);
     }
 }
